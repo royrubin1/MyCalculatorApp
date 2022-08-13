691a6c109e7966033f648312134c2d7cfbf8e79b
Roy Rubin <royrubin@protonmail.com>
2022-08-13 11:49:00 +0200

    Class96

--- app/src/main/java/roy/example/mycalculator/MainActivity.kt
+++ app/src/main/java/roy/example/mycalculator/MainActivity.kt
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import  android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
