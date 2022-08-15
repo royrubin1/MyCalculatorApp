ace76afca742fd2690160d92fd619d68da862f76
Roy Rubin <royrubin@protonmail.com>
2022-08-15 02:34:46 +0200

    Class99

--- app/src/main/java/roy/example/mycalculator/MainActivity.kt
+++ app/src/main/java/roy/example/mycalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import  android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         tvInput?.append((view as Button).text)
         lastNumeric = true
         lastDot = false
+
     }
 
     fun onClear(view: View) {
@@ -35,6 +37,59 @@ class MainActivity : AppCompatActivity() {
             lastNumeric = false
             lastDot = true
         }
+
+
+
     }
 
+    fun onOperator(view: View) {
+        tvInput?.text?.let{
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+            }catch (e: ArithmeticException){
+                println("Arithmetic Exception")
+            }
+        }
+    }
+
+    private fun isOperatorAdded(Value: String) : Boolean {
+        return if (Value.startsWith("-")) {
+            false
+        }else{
+            Value.contains("/")
+                    || Value.contains("*")
+                    || Value.contains("-")
+                    || Value.contains("+")
+        }
+    }
 }
